d347eee2c7570feb57c8d8685c7dc4579fb627b8
lucy_1215 <olivia258@sungkyul.ac.kr>
2022-11-27 22:49:57 +0900

    [Bronze V] Title: 영수증, Time: 224 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int count = sc.nextInt();
+        
+        for(int i=0;i<count;i++) {
+            int price = sc.nextInt();
+            int num = sc.nextInt();
+            total = total - (price*num);
+        }
+        
+        if(total == 0) {
+            System.out.print("Yes");
+        }else {
+            System.out.print("No");
+        }
+        
+    }
+
+}
