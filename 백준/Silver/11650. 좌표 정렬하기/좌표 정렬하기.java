29d6c0561b951970228fb664d452c997f63347c0
lucy_1215 <olivia258@sungkyul.ac.kr>
2022-11-30 19:24:50 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2336 ms, Memory: 193752 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Main{
+	 public static void main(String[] args) throws NumberFormatException, IOException {
+		 
+		 Scanner sc = new Scanner(System.in);	     
+		 int t = sc.nextInt();
+	     
+		 int[][] ar = new int[t][2];
+		 
+		 for(int i=0;i<t;i++) {
+			 ar[i][0] = sc.nextInt();
+			 ar[i][1] = sc.nextInt();
+		 }
+		 
+		 Arrays.sort(ar, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// TODO Auto-generated method stub
+				if(o1[0] == o2[0]) {
+					return o1[1] - o2[1];
+				}else {
+					return o1[0] - o2[0];
+				}
+			}
+		});
+		 
+		 for(int i=0;i<t;i++) {
+			 System.out.println(ar[i][0] + " " +ar[i][1]);
+		 }
+
+	 }
+}
