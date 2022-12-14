5a781821e79844bf5aaced8eceb667977de3e6f3
lucy_1215 <olivia258@sungkyul.ac.kr>
2022-12-14 16:38:18 +0900

    [Silver II] Title: 이항 계수 2, Time: 220 ms, Memory: 21512 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main{
+	
+	static int[][] dp;
+	
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int m = sc.nextInt();
+    	dp = new int[n+1][m+1];
+    	
+    	
+    	System.out.println(BC(n,m)%10007);
+        	
+    	
+    }
+    
+    public static int BC(int n, int m) {
+    	if(dp[n][m] > 0) {
+    		return dp[n][m];
+    	}
+    	
+    	if(n == m || m==0 ) {
+    		return dp[n][m] = 1;
+    	}
+    	return dp[n][m] = (BC(n-1,m-1) + BC(n-1,m)) %10007;
+    	
+    }
+}
+
