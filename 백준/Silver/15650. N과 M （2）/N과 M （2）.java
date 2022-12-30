23df5b0447f50959d89e45ef970d494f77f21179
lucy_1215 <olivia258@sungkyul.ac.kr>
2022-12-30 18:24:22 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	static int n,m;
+	static int a[];
+	static StringBuilder sb = new StringBuilder();
+	
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+    	
+    	a = new int[m];
+    	
+    	backtracking(1,0);
+    	System.out.println(sb);
+    
+    	
+    }
+    
+    public static void backtracking(int at, int index) {
+    	if(index == m) {
+    		for(int i=0;i<m;i++) {
+    			sb.append(a[i]).append(" ");
+    		}
+    		sb.append('\n');
+    		return;
+    	}
+    	
+    	for(int i=at;i<=n;i++) {
+    		a[index] = i;
+    		backtracking(i+1,index+1);
+    	}
+    }
+    
+
+}
+
