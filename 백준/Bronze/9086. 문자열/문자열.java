50d5354e1b9f2477febaa695ba13b908d64c7a27
lucy1215 <olivia258@sungkyul.ac.kr>
2023-08-13 09:48:21 +0900

    [Bronze V] Title: 문자열, Time: 204 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			String s = sc.next();
+			System.out.print(s.charAt(0));
+			System.out.println(s.charAt(s.length()-1));
+		}
+	}
+
+}
