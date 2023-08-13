9c7abbae53fcc6ef258340a6a07673bff4034cbb
lucy1215 <olivia258@sungkyul.ac.kr>
2023-08-13 14:36:13 +0900

    [Bronze V] Title: 직사각형, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int width = sc.nextInt();
+		int height = sc.nextInt();
+		
+		System.out.println(width * height);
+	}
+}
