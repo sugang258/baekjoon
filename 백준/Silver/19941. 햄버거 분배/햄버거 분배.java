23e767611659773b03ef35926512d9f2ac793835
lucy1215 <olivia258@sungkyul.ac.kr>
2024-06-15 13:05:15 +0900

    [Silver III] Title: 햄버거 분배, Time: 216 ms, Memory: 18572 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -14,11 +14,8 @@ public class Main {
         //p일때, 앞에서 부터 탐색 - H이고 check[i] == false => 햄버거 먹을 수 있음
         for (int i = 0; i < n; i++) {
             if(str.charAt(i) == 'P') {
-                int start = i - k;
-                int end = i + k;
-
-                if(start < 0) start = 0;
-                if(end > n-1) end = n-1;
+               int start = Math.max(i-k,0);
+               int end = Math.min(i + k,n-1);
 
                 for (int j = start; j <= end; j++) {
                     if(str.charAt(j) == 'H' && check[j] == false){
