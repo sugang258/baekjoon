e7078188bfb1afff05ea29aa93e335920c47f265
lucy1215 <olivia258@sungkyul.ac.kr>
2024-08-05 21:12:50 +0900

    [Silver V] Title: 타일 장식물, Time: 104 ms, Memory: 14256 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		long [] dp = new long[81];
+
+		
+		dp[1] = 4;
+		dp[2] = 6;
+
+		for(int i=3; i<=n; i++)
+			dp[i] = dp[i-1] + dp[i-2];
+		
+		System.out.println(dp[n]);
+		
+	}
+}
